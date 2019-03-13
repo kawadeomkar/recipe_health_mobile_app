88f4024ba43df965c0a1d90786dca4d9285236d4
Conner Mullen <cdmullen@uci.edu>
2019-03-12 22:14:02 -0700

    Load Values from DB

--- app/src/main/java/com/example/recipe_app/AccountFragment.java
+++ app/src/main/java/com/example/recipe_app/AccountFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.android.gms.common.util.CollectionUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,6 +49,67 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         getView().findViewById(R.id.save_button).setOnClickListener(this);
+        String email = "";
+        Bundle bundle = this.getArguments();
+        email = bundle.getString("email");
+        DocumentReference currentInfo = db.collection("users").document(email);
+        currentInfo.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                EditText temp;
+                Spinner temp2;
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // Get the data back
+                        Object doc = document.getData();
+
+                        // Set age text field to age in database
+                        temp = getView().findViewById(R.id.editAge);
+                        temp.setText(((Map) doc).get("age").toString());
+
+                        // Set weight text field to weight in database
+                        temp = getView().findViewById(R.id.editWeight);
+                        temp.setText(((Map) doc).get("weight").toString());
+
+                        // Set height text field to height in database
+                        temp = getView().findViewById(R.id.editHeight);
+                        temp.setText(((Map) doc).get("height").toString());
+
+                        // Set gender spinner field to gender in database
+                        temp2 = getView().findViewById(R.id.editGender);
+                        String gen = ((Map) doc).get("gender").toString();
+                        if (gen.equals("Male")) {
+                            temp2.setSelection(0);
+                        }
+                        else {
+                            temp2.setSelection(1);
+                        }
+
+                        // Set weight_goal spinner field to weight_gaol in database
+                        temp2 = getView().findViewById(R.id.editWeightGoal);
+                        String goal = ((Map) doc).get("weight_goal").toString();
+                        if (goal.equals("Bulk up!")) {
+                            temp2.setSelection(0);
+                        }
+                        else if (goal.equals("Cut fat!")) {
+                            temp2.setSelection(1);
+                        }
+                        else {
+                            temp2.setSelection(2);
+                        }
+
+                        Log.d("Register.java", "Doc data: " + document.getData());
+                    }
+                    else {
+                        Log.d("Register.java", "No such doc");
+                    }
+                }
+                else {
+                    Log.d("Register.java", "get failed with " + task.getException());
+                }
+            }
+        });
     }
 
     // Check which listener was called
@@ -86,9 +154,13 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
 
                 // TODO: get the variable email, similar to RegistrationInfo.java
                 //getActivity().getIntent().getStringExtra("email_from_reg"); DOESNT WORK
+                // get bundle arguments
+                String email = "";
+                Bundle bundle = this.getArguments();
+                email = bundle.getString("email");
 
 
-                //db.collection("users").document(email).update(userMap);
+                db.collection("users").document(email).update(userMap);
 
         }
     }
