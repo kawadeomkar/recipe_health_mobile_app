5f55c95e2cc8c6eae9ac59b4dc9380bede0f4145
Isabelle Nguyen <itnguye1@uci.edu>
2019-03-13 23:02:49 -0700

    add ingredients functionality

--- app/src/main/java/com/example/recipe_app/FridgeFragment.java
+++ app/src/main/java/com/example/recipe_app/FridgeFragment.java
@@ -21,11 +21,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
